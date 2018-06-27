a35d03d31282e21245b614a1da0c7c10bd8975db
Lorenzo Covarrubias Jr <lorenzocovarrubiasjr@gmail.com>
2018-06-26 23:03:38 -0500

    Added quick plan of action to ZillowSearcher.rb

--- lib/ZillowSearcher.rb
+++ lib/ZillowSearcher.rb
@@ -1,5 +1,10 @@
 require "ZillowSearcher/version"
 
+# --Needs to run program and give 3 options - do you want to see homes for sale - homes for rent - find an agent
+# --Need a class for Homes for sale that will store all the info on homes for sale -address, price, details
+# --Need a class for Homes for rent that will store all the info on homes for rent -address, price, details
+# -- Need a class for Agents that will store their info - name, rating, phone number
+
 module ZillowSearcher
   # Your code goes here...
 end
